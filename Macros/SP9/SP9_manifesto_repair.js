1a35d8ec89c8df4b96a3c166c81009b27900c471
Chris Swope <sswwooppee@gmail.com>
2013-04-30 00:02:07 -0500

    Update SP9_manifesto_repair.js
    it gets it's xid info from the content collection of the instance in which it's running.

--- Macros/SP9/SP9_manifesto_repair.js
+++ Macros/SP9/SP9_manifesto_repair.js
@@ -14,6 +14,101 @@ var fixManifestoInstitionItems = function() {
 	var e = 0;
 	var contentAreas = [];
 	var i = 0;
+	var artifactLinks = [];
+	
+	var captureArtifactLinks = function() {
+		var macroCode = "";
+		var e = 0;
+		var artifactRows = [];
+		var artifactLinks = [];
+		var xID = "";
+		var artifact = "";
+	    
+	    try {
+            macroCode = "SET !VAR1 {{!URLCURRENT}}\n";
+            macroCode += "TAB OPEN\n";
+            macroCode += "TAB T=2\n";
+            macroCode += "URL GOTO={{!VAR1}}\n";
+            e = iimPlay("CODE:" + macroCode);
+            
+            macroCode = "TAB T=1\nFRAME NAME=\"nav\"\n";
+            macroCode += "TAG POS=1 TYPE=SPAN ATTR=TXT:Content<SP>Collection\n";
+            e = iimPlay("CODE:" + macroCode);
+            
+            macroCode = "FRAME NAME=\"WFS_Navigation\"\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:Institution<SP>Content\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:institution\n";
+            macroCode += "FRAME NAME=\"WFS_Files\"\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:*_Manifesto\n";
+            e = iimPlay("CODE:" + macroCode);
+            
+            macroCode = "TAB T=1\nFRAME NAME=\"WFS_Files\"\n";
+            macroCode += "TAG POS=1 TYPE=TBODY ATTR=ID:listContainer_databody EXTRACT=HTM\n";
+            e = iimPlay("CODE:" + macroCode);
+            extract = iimGetLastExtract();
+            
+            artifactRows = extract.match(/<tr[\s\S]+?<\/tr>/g);
+            
+            for (j = 0; j < artifactRows.length; j++) {
+                if (artifactRows[j].search(/orientation\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/orientation\.html/i);
+                }
+                if (artifactRows[j].search(/learner_expectations\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/learner_expectations\.html/i);
+                }
+                if (artifactRows[j].search(/grading_explanation\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/grading_explanation\.html/i);
+                }
+                if (artifactRows[j].search(/disability_services_statement\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/disability_services_statement\.html/i);
+                }
+                if (artifactRows[j].search(/courseroom_tour\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/i)[1];
+                    title = artifactRows[j].match(/courseroom_tour\.html/i);
+                }
+                if (artifactRows[j].search(/course_plan\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/course_plan\.html/i);
+                }
+                artifact = "<a artifacttype=\"html\" href=\"http://@X@EmbeddedFile.requestUrlStub@X@/bbcswebdav/xid-" + xID + "_1\" target=\"_blank\" alt=\"\">" + title + "</a>";
+                artifactLinks.push(artifact);
+            }
+            
+            macroCode = "FRAME NAME=\"WFS_Navigation\"\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:Institution<SP>Content\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:institution\n";
+            macroCode += "FRAME NAME=\"WFS_Files\"\n";
+            macroCode += "TAG POS=1 TYPE=A ATTR=TXT:*_Print\n";
+            e = iimPlay("CODE:" + macroCode);
+            
+            macroCode = "TAB T=1\nFRAME NAME=\"WFS_Files\"\n";
+            macroCode += "TAG POS=1 TYPE=TBODY ATTR=ID:listContainer_databody EXTRACT=HTM\n";
+            e = iimPlay("CODE:" + macroCode);
+            extract = iimGetLastExtract();
+            
+            artifactRows = extract.match(/<tr[\s\S]+?<\/tr>/g);
+            
+            for (j = 0; j < artifactRows.length; j++) {
+                if (artifactRows[j].search(/oob_print_manifesto\.html/i) != -1) {
+                    xID = artifactRows[j].match(/id=\"(\d+)_1_xythosFileSize\"/)[1];
+                    title = artifactRows[j].match(/oob_print_manifesto\.html/i);
+                }
+                artifact = "<a artifacttype=\"html\" href=\"http://@X@EmbeddedFile.requestUrlStub@X@/bbcswebdav/xid-" + xID + "_1\" target=\"_blank\" alt=\"\">" + title + "</a>";
+                artifactLinks.push(artifact);
+            }
+            
+            macroCode = "TAB CLOSE\n";
+            e = iimPlay("CODE:" + macroCode);
+            return artifactLinks
+		} catch(err) {
+		    alert(err + "\nSomething went wrong with captureArtifactLinks");
+		}
+		
+	}
 	
 	var extractLNav = function() {
 		var macroCode = "";
@@ -63,23 +158,39 @@ var fixManifestoInstitionItems = function() {
 		}
     }
     
+    var getArtifact = function(searchPattern, artifactLinks) {
+        var artifact = "";
+        
+        try {
+            for (j = 0; j < artifactLinks.length; j++) {
+                if (artifactLinks[j].search(searchPattern) != -1) {
+                    artifact = artifactLinks[j];
+                }
+            }
+            return artifact
+        } catch(err) {
+            alert(err + "\nSomething went wrong with getArtifact");
+        }
+    }
+    
     var fixPrint = function(contentLIs) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contentLIs = [];
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a artifacttype=\"html\" href=\"http://prsa.capella.edu/bbcswebdav/xid-1609904_1\" target=\"_blank\" alt=\"\">oob_print_manifesto.html</a>";
+        var artifact = "";
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/oob_print_manifesto\.html/i, artifactLinks);
             contextualMenuIdNumber = contentLIs[0].match(/cmlink_(\w{32})/)[1];
         
             macroCode = "TAB T=1\nFRAME NAME=\"content\"\n";
             macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
             macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
             macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-            macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+            macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
             macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:top_Submit&&VALUE:Submit";
             e = iimPlay("CODE:" + macroCode);
             return
@@ -88,17 +199,18 @@ var fixManifestoInstitionItems = function() {
 		}
     }
     
-    var fixCoursePlan = function() {
+    var fixCoursePlan = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609397_1\" target=\"_blank\" artifacttype=\"html\">Course_Plan.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
-        
+            artifact = getArtifact(/course_plan\.html/i, artifactLinks);
+            
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Required Course Plan/) != -1) {
                     contextualMenuIdNumber = contentLIs[j].match(/cmlink_(\w{32})/)[1];
@@ -106,7 +218,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:manageAssignmentForm ATTR=ID:content_desc_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:manageAssignmentForm ATTR=ID:content_desc_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:manageAssignmentForm ATTR=NAME:top_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -120,16 +232,17 @@ var fixManifestoInstitionItems = function() {
         }
     }
     
-    var fixLearnerExpectations = function() {
+    var fixLearnerExpectations = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609902_1\" target=\"_blank\" artifacttype=\"html\">expectations.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/learner_expectations\.html/i, artifactLinks);
             
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Learner Expectations/) != -1) {
@@ -138,7 +251,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:bottom_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -152,16 +265,17 @@ var fixManifestoInstitionItems = function() {
         }
     }
     
-    var fixOrientation = function() {
+    var fixOrientation = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609903_1\" target=\"_blank\" artifacttype=\"html\">orientation.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/orientation\.html/i, artifactLinks);
             
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Orientation/) != -1) {
@@ -170,7 +284,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:bottom_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -184,16 +298,17 @@ var fixManifestoInstitionItems = function() {
         }
     }
     
-    var fixCourseroomTour = function() {
+    var fixCourseroomTour = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609399_1\" target=\"_blank\" artifacttype=\"html\">Courseroom_Tour.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/courseroom_tour\.html/i, artifactLinks);
             
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Courseroom Tour/) != -1) {
@@ -202,7 +317,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:bottom_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -216,16 +331,17 @@ var fixManifestoInstitionItems = function() {
         }
     }
     
-    var fixExplanationOfGrading = function() {
+    var fixExplanationOfGrading = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609901_1\" target=\"_blank\" artifacttype=\"html\">manifesto_explanation.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/grading_explanation\.html/i, artifactLinks);
             
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Explanation of Grading/) != -1) {
@@ -234,7 +350,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:bottom_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -248,16 +364,17 @@ var fixManifestoInstitionItems = function() {
         }
     }
     
-    var fixDisabilityServicesStatement = function() {
+    var fixDisabilityServicesStatement = function(artifactLinks) {
         var macroCode = "";
         var e = 0;
         var extract = "";
         var contextualMenuIdNumber = "";
-        var printArtifact = "<a href=\"http://prsa.capella.edu/bbcswebdav/xid-1609400_1\" target=\"_blank\" artifacttype=\"html\">Disability_Services_Statement.html</a>";
+        var artifact = "";
         var j = 0;
         
         try {
             contentLIs = captureContentAreas();
+            artifact = getArtifact(/disability_services_statement\.html/i, artifactLinks);
             
             for (j = 0; j < contentLIs.length; j++) {
                 if (contentLIs[j].search(/Disability Services Statement/) != -1) {
@@ -266,7 +383,7 @@ var fixManifestoInstitionItems = function() {
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:cmlink_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=A ATTR=ID:edit_" + contextualMenuIdNumber + "\n";
                     macroCode += "TAG POS=1 TYPE=IMG ATTR=SRC:http://*.capella.edu/images/ci/textboxeditor/ed_html.gif\n";
-                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(printArtifact) + "\n";
+                    macroCode += "TAG POS=1 TYPE=TEXTAREA FORM=NAME:the_form ATTR=ID:htmlData_text CONTENT=" + addIIMSpaces(artifact) + "\n";
                     macroCode += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:the_form ATTR=NAME:bottom_Submit&&VALUE:Submit\n";
                     e = iimPlay("CODE:" + macroCode);
                     if (e != 1) {
@@ -282,20 +399,20 @@ var fixManifestoInstitionItems = function() {
     
     try {
         contentAreas = extractLNav();
-    
+        artifactLinks = captureArtifactLinks();
+        
         for (i = 0; i < contentAreas.length; i++) {
             macroCode = "TAB T=1\nFRAME NAME=\"content\"\n";
             macroCode += "TAG POS=1 TYPE=SPAN ATTR=TXT:" + addIIMSpaces(contentAreas[i]) + "\n";
             e = iimPlay("CODE:" + macroCode);
             
             fixPrint();
-            fixCoursePlan();
-            fixLearnerExpectations();
-            fixOrientation();
-            fixCourseroomTour();
-            fixExplanationOfGrading();
-            //fixDisabilityServicesStatement();
-            
+            fixCoursePlan(artifactLinks);
+            fixLearnerExpectations(artifactLinks);
+            fixOrientation(artifactLinks);
+            fixCourseroomTour(artifactLinks);
+            fixExplanationOfGrading(artifactLinks);
+            fixDisabilityServicesStatement(artifactLinks);
         }
         return
     } catch(err) {
